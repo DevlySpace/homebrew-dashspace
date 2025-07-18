543eae8e0fb4d8aea5896debf4ef65f1c01c222a
Quentin Devly <quentin.hattstadt@devly.space>
2025-07-19 01:28:52 +0200

    Update DashSpace CLI to v0.0.5

--- Formula/dashspace.rb
+++ Formula/dashspace.rb
@@ -2,14 +2,14 @@ class Dashspace < Formula
   desc "Official DashSpace CLI for creating and publishing modules"
   homepage "https://dashspace.space"
   license "MIT"
-  version "0.0.4"
+  version "0.0.5"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/devlyspace/devly-cli/releases/download/0.0.4/dashspace-darwin-arm64"
+      url "https://github.com/devlyspace/devly-cli/releases/download/0.0.5/dashspace-darwin-arm64"
       sha256 "d0aef668a3798d5baaf8ff6f62c8484a73b63284ca535f80560fce9bee914f6b"
     else
-      url "https://github.com/devlyspace/devly-cli/releases/download/0.0.4/dashspace-darwin-amd64"
+      url "https://github.com/devlyspace/devly-cli/releases/download/0.0.5/dashspace-darwin-amd64"
       sha256 "27154461a494c2956eb17e5d2e229a49736533c2046b59f24ba13c9eb1b2f3b3"
     end
   end
@@ -30,6 +30,6 @@ class Dashspace < Formula
 
   test do
     system "#{bin}/dashspace", "--version"
-    assert_match "dashspace version 0.0.4", shell_output("#{bin}/dashspace --version")
+    assert_match "dashspace version 0.0.5", shell_output("#{bin}/dashspace --version")
   end
 end
