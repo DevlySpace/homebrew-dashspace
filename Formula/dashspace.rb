f2374fcd75859761d8f21ba81b931bbef9ef69ab
ChargemapQuentin <quentin.hattstadt@chargemap.com>
2025-07-19 00:04:55 +0200

    :sparkles: Init dashspace cli tap

---
+++ Formula/dashspace.rb
@@ -0,0 +1,35 @@
+class Dashspace < Formula
+  desc "Official DashSpace CLI for creating and publishing modules"
+  homepage "https://dashspace.space"
+  license "MIT"
+  version "1.0.0"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/dashspace/cli/releases/download/v1.0.0/dashspace-darwin-arm64"
+      sha256 "PLACEHOLDER_ARM64_SHA"
+    else
+      url "https://github.com/dashspace/cli/releases/download/v1.0.0/dashspace-darwin-amd64"
+      sha256 "PLACEHOLDER_AMD64_SHA"
+    end
+  end
+
+  def install
+    if Hardware::CPU.arm?
+      bin.install "dashspace-darwin-arm64" => "dashspace"
+    else
+      bin.install "dashspace-darwin-amd64" => "dashspace"
+    end
+
+    begin
+      generate_completions_from_executable(bin/"dashspace", "completion")
+    rescue
+      # Ignore if completion not supported
+    end
+  end
+
+  test do
+    system "#{bin}/dashspace", "--version"
+    assert_match "dashspace version 1.0.0", shell_output("#{bin}/dashspace --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,38 @@
+# DashSpace Homebrew Tap
+
+Official Homebrew tap for DashSpace CLI.
+
+## 🍺 Installation
+
+```bash
+brew tap dashspace/dashspace
+brew install dashspace
+```
+
+## 🔄 Update
+
+```bash
+brew upgrade dashspace
+```
+
+## 📦 Available Formulae
+
+- **dashspace** - Official DashSpace CLI for creating and publishing modules
+
+## 🔗 Links
+
+- [DashSpace Website](https://dashspace.space)
+- [CLI Repository](https://github.com/dashspace/cli)
+- [Documentation](https://docs.dashspace.space)
+
+## 🐛 Issues
+
+If you encounter any issues with the Homebrew installation, please report them in the [main CLI repository](https://github.com/dashspace/cli/issues).
+
+## 📋 Manual Installation
+
+If you prefer not to use the tap:
+
+```bash
+brew install https://raw.githubusercontent.com/dashspace/homebrew-dashspace/main/Formula/dashspace.rb
+```
