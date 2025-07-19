9af9c6d7d4e7969ab9a472931ffd150240cd6399
Quentin Devly <quentin.hattstadt@devly.space>
2025-07-19 02:04:40 +0200

    Update DashSpace CLI to 0.0.9

--- Formula/dashspace.rb
+++ Formula/dashspace.rb
@@ -2,34 +2,29 @@ class Dashspace < Formula
   desc "Official DashSpace CLI for creating and publishing modules"
   homepage "https://dashspace.space"
   license "MIT"
-  version "0.0.7"
+  version "0.0.9"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/devlyspace/devly-cli/releases/download/0.0.7/dashspace-0.0.7-darwin-arm64"
-      sha256 "f317c56bdae997d305c30b20c6eb30504357a4fccc11f13522c3085acf8578e4"
+      url "https://github.com/devlyspace/devly-cli/releases/download/0.0.9/dashspace-0.0.9-darwin-arm64.tar.gz"
+      sha256 "a35c61512ff037761396932eba32ce1fffb2998caa6d6b179a9517cd2083fc0d"
     else
-      url "https://github.com/devlyspace/devly-cli/releases/download/0.0.7/dashspace-0.0.7-darwin-amd64"
-      sha256 "9c9eb6eb6ac5b72d315bc74d11f24ef507c6e8da283714c64c580bacf28cd665"
+      url "https://github.com/devlyspace/devly-cli/releases/download/0.0.9/dashspace-0.0.9-darwin-amd64.tar.gz"
+      sha256 "7ce1283f4b6e55afdbd0cfe013c9430c65c6402bc2dacd5018d3f88e148fe846"
     end
   end
 
   def install
-    if Hardware::CPU.arm?
-      bin.install "dashspace-0.0.7-darwin-arm64" => "dashspace"
-    else
-      bin.install "dashspace-0.0.7-darwin-amd64" => "dashspace"
-    end
+    bin.install "dashspace" => "dashspace"
 
     begin
       generate_completions_from_executable(bin/"dashspace", "completion")
     rescue
-      # Ignore if completion not supported
     end
   end
 
   test do
     system "#{bin}/dashspace", "--version"
-    assert_match "dashspace version 0.0.7", shell_output("#{bin}/dashspace --version")
+    assert_match "dashspace version 0.0.9", shell_output("#{bin}/dashspace --version")
   end
 end
